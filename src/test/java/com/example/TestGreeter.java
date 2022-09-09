7e4d81f5791e6cbf80e5bc8d42d9fd65d4d88e37
root <root@ip-172-31-40-93.ap-south-1.compute.internal>
2022-09-09 07:20:20 +0000

    this is test wrong test case

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -23,8 +23,9 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
+    String anotherOne="Hello";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone), containsString(anotherOne));
   }
 
   @Test
